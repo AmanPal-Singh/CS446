9fe153ce71a9e2474c6d43a4259a4b2ed10eb2b7
Aman Singh <amanpal_singh@outlook.com>
2023-03-12 21:15:47 -0400

    Mock data for routines

--- app/src/main/java/com/example/goosebuddy/ui/screens/Routines.kt
+++ app/src/main/java/com/example/goosebuddy/ui/screens/Routines.kt
@@ -6,10 +6,10 @@ import androidx.compose.runtime.Composable
 
 sealed class RoutineItem(var title: String, var progress: Int) {
     object Skincare: RoutineItem("Skincare", 100)
-    object Skincare: RoutineItem("Fitness", 75)
-    object Skincare: RoutineItem("Yoga", 0)
-    object Skincare: RoutineItem("Cleaning", 50)
-    object Skincare: RoutineItem("Study", 25)
+    object Fitness: RoutineItem("Fitness", 75)
+    object Yoga: RoutineItem("Yoga", 0)
+    object Cleaning: RoutineItem("Cleaning", 50)
+    object Study: RoutineItem("Study", 25)
 }
 
 @Composable
