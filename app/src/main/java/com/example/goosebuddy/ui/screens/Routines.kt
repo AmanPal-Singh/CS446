2c12c87252c0ea7d74956a4f017a2570943bbddb
Julia Zhang <juliazhang1108@gmail.com>
2023-03-12 22:07:15 -0400

    routines list

--- app/src/main/java/com/example/goosebuddy/ui/screens/Routines.kt
+++ app/src/main/java/com/example/goosebuddy/ui/screens/Routines.kt
@@ -1,16 +1,25 @@
 package com.example.goosebuddy.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import android.widget.Space
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.semantics.Role.Companion.Checkbox
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.goosebuddy.ui.theme.Grey
+import com.example.goosebuddy.ui.theme.Red
+import com.example.goosebuddy.ui.theme.White
 
 
 sealed class RoutineItem(var title: String, var progress: Int) {
@@ -35,20 +44,34 @@ val items = listOf(
 
 @Composable
 fun Routines() {
-    Surface() {
-        Column {
-            items.forEach { item ->
-                RoutineBlock(item = item)
-            }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(2.dp, Red, RectangleShape)
+            .background(Grey),
+    ) {
+        items.forEach { item ->
+            RoutineBlock(item = item)
         }
     }
+
 }
 
 @Composable
 fun RoutineBlock(item: RoutineItem) {
-    Surface() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(7.dp)
+    ) {
         Row(
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp)
         ) {
             Checkbox(
                 checked = item.progress == 100,
@@ -58,14 +81,17 @@ fun RoutineBlock(item: RoutineItem) {
             )
             Column {
                 Text(item.title)
+                Spacer(Modifier.height(10.dp))
                 LinearProgressIndicator(progress = item.progress/100f)
+                Spacer(Modifier.height(10.dp))
             }
 
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(
                     imageVector = Icons.Default.KeyboardArrowRight,
                     contentDescription = "More",
-                    tint = Grey
+                    tint = Grey,
+                    modifier = Modifier.size(50.dp)
                 )
             }
         }
