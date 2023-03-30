ec719e6049a218621bd87ac3af1e35e9d77a9048
Julia Zhang <juliazhang1108@gmail.com>
2023-03-30 14:58:03 -0400

    add drawer

--- app/src/main/java/com/example/goosebuddy/ui/shared/components/topbar/TopBar.kt
+++ app/src/main/java/com/example/goosebuddy/ui/shared/components/topbar/TopBar.kt
@@ -6,20 +6,32 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.example.goosebuddy.ui.theme.White
+import kotlinx.coroutines.launch
 
 @Composable
-fun TopBar() {
+fun TopBar(scaffoldState: ScaffoldState) {
+    val scope = rememberCoroutineScope()
     Row(
-        horizontalArrangement = Arrangement.End,
+        horizontalArrangement = Arrangement.Start,
         modifier = Modifier.fillMaxWidth()
             .background(White)
     ) {
-        IconButton(onClick = { /*TODO*/ }) {
+        IconButton(
+            onClick = {
+                scope.launch {
+                    scaffoldState.drawerState.apply {
+                        if (isClosed) open() else close()
+                    }
+                }
+            }
+        ) {
             Icon(imageVector = Icons.Default.Menu, contentDescription = "menu")
         }
     }
