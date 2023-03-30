ddce92a334c15e5371457b9b541ac29b19f610a8
Aman Singh <amanpal_singh@outlook.com>
2023-03-30 18:32:22 -0400

    Implemented the singleton class for the db

--- app/src/main/java/com/example/goosebuddy/AppDatabase.kt
+++ app/src/main/java/com/example/goosebuddy/AppDatabase.kt
@@ -1,9 +1,11 @@
 package com.example.goosebuddy
 
+import android.content.Context
 import androidx.room.AutoMigration
 import com.example.goosebuddy.dao.RoutinesDao
 import com.example.goosebuddy.dao.WeekdayDataDao
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.goosebuddy.dao.HabitsDao
 import com.example.goosebuddy.models.WeekdayData
@@ -17,5 +19,20 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun routinesDao(): RoutinesDao;
     abstract fun weekdayDataDao(): WeekdayDataDao;
     abstract fun habitsDao(): HabitsDao;
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        @Synchronized
+        fun createInstance(context: Context): AppDatabase {
+            if(instance == null)
+                instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java, "database-name"
+                ).allowMainThreadQueries().fallbackToDestructiveMigrationFrom(1).build()
+
+            return instance!!
+
+        }}
 }
 
