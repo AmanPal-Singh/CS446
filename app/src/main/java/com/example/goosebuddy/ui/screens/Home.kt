0624e005565bd9c47947afb3757b98b7a5a6ef50
Julia Zhang <juliazhang1108@gmail.com>
2023-04-02 15:53:27 -0400

    add date

--- app/src/main/java/com/example/goosebuddy/ui/screens/Home.kt
+++ app/src/main/java/com/example/goosebuddy/ui/screens/Home.kt
@@ -13,14 +13,10 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,9 +25,10 @@ import com.example.goosebuddy.R
 import com.example.goosebuddy.ui.theme.Beige
 import com.example.goosebuddy.ui.theme.LightBlue
 import com.example.goosebuddy.ui.theme.LightGrey
-import androidx.compose.runtime.getValue
-
-import androidx.compose.runtime.livedata.observeAsState
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 val options = arrayOf(
     "Got it.",
@@ -49,12 +46,43 @@ fun Home() {
             .fillMaxHeight()
             .background(LightGrey),
     ) {
-        Spacer(modifier = Modifier.size(50.dp))
+        Spacer(modifier = Modifier.size(30.dp))
+        DatedGreeting()
+        Spacer(modifier = Modifier.size(30.dp))
         SpeechBubble()
         Spacer(modifier = Modifier.size(30.dp))
         Goose()
-        Spacer(modifier = Modifier.size(30.dp))
         SpeechOptions(options = options)
+    }
+}
+
+@Composable
+fun DatedGreeting() {
+    val current = LocalDateTime.now()
+    val hour = current.hour
+    val formatter = DateTimeFormatter.ofPattern("LLL dd, yyyy")
+    val formatted = current.format(formatter)
+
+    var greeting = "Good Morning!"
+
+    if (hour in 0..11){
+        greeting = "Good Morning!"
+    } else if(hour in 12..16){
+        greeting = "Good Afternoon!"
+    } else if(hour in 17..20){
+        greeting = "Good Evening!"
+    } else if(hour in 21..23){
+        greeting = "Good Night!"
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(formatted)
+        Text(greeting)
     }
 }
 
