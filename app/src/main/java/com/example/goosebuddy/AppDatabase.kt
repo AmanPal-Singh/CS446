40715a9b32982e08c3531ed51a8c8742677cdf24
Vinojan Veluppilai <vinojanvel@gmail.com>
2023-04-02 20:09:24 -0400

    Generalize db fallback for future migrations

--- app/src/main/java/com/example/goosebuddy/AppDatabase.kt
+++ app/src/main/java/com/example/goosebuddy/AppDatabase.kt
@@ -29,7 +29,7 @@ abstract class AppDatabase : RoomDatabase() {
                 instance = Room.databaseBuilder(
                     context,
                     AppDatabase::class.java, "database-name"
-                ).allowMainThreadQueries().fallbackToDestructiveMigrationFrom(1).build()
+                ).allowMainThreadQueries().fallbackToDestructiveMigration().build();
 
             return instance!!
 
