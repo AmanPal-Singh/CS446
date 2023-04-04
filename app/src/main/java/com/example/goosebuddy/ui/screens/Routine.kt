b08c88a421656771424ab1f06293d45677cce2de
Julia Zhang <juliazhang1108@gmail.com>
2023-04-03 21:13:17 -0400

    add delete buttons

--- app/src/main/java/com/example/goosebuddy/ui/screens/Routine.kt
+++ app/src/main/java/com/example/goosebuddy/ui/screens/Routine.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.goosebuddy.R
+import com.example.goosebuddy.ui.shared.components.DeleteButton
 import com.example.goosebuddy.ui.shared.components.Goose
 import com.example.goosebuddy.ui.shared.components.SpeechBubble
 import kotlinx.coroutines.CoroutineScope
@@ -95,6 +96,7 @@ fun Routine(name: String, subroutines: List<Subroutine>, navController: NavHostC
                                 description = subroutine.description,
                                 completed = subroutine.completed,
                                 duration = subroutine.duration,
+                                editingEnabled = editingEnabled,
                                 elevation = elevation.value
                             )
                         }
@@ -151,6 +153,7 @@ fun SubroutineCard(
     description: String,
     duration: Duration,
     completed: Boolean,
+    editingEnabled: MutableState<Boolean>,
     elevation: Dp
 ) {
     val textStyle = if (completed) TextStyle(textDecoration = TextDecoration.LineThrough) else TextStyle()
@@ -164,8 +167,7 @@ fun SubroutineCard(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(10.dp)
-            ,
+                .padding(10.dp),
         ) {
             Column() {
                 Text(
@@ -178,9 +180,14 @@ fun SubroutineCard(
                     style = textStyle
                 )
             }
-            Text(
-                duration.toString(),
-            )
+            if (!editingEnabled.value) {
+                Text(
+                    duration.toString(),
+                )
+            } else {
+                DeleteButton(onDelete = { /** TODO */ })
+            }
+
         }
     }
 }
