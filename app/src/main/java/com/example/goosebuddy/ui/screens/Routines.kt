f2621e0a10cd7306e4fdfc154a7cbe871bf93471
Julia Zhang <juliazhang1108@gmail.com>
2023-04-04 22:05:48 -0400

    remove imports

--- app/src/main/java/com/example/goosebuddy/ui/screens/Routines.kt
+++ app/src/main/java/com/example/goosebuddy/ui/screens/Routines.kt
@@ -18,17 +18,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
-import androidx.room.RoomDatabase
 import com.example.goosebuddy.AppDatabase
 import com.example.goosebuddy.R
 import com.example.goosebuddy.models.Routines
