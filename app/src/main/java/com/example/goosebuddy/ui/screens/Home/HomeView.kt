cfa92037db48125f8338305c16547575b0059d1b
Julia Zhang <juliazhang1108@gmail.com>
2023-04-09 01:42:17 -0400

    mvvm home

--- app/src/main/java/com/example/goosebuddy/ui/screens/Home/HomeView.kt
+++ app/src/main/java/com/example/goosebuddy/ui/screens/Home/HomeView.kt
@@ -5,19 +5,18 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.goosebuddy.AppDatabase
+import com.example.goosebuddy.ui.screens.Home.HomeViewModel
+import com.example.goosebuddy.ui.screens.Utility.formatTime
 import com.example.goosebuddy.ui.shared.components.DefaultGoose
-import com.example.goosebuddy.ui.shared.components.Goose
 import com.example.goosebuddy.ui.shared.components.SpeechBubble
 import com.example.goosebuddy.ui.theme.Beige
 import com.example.goosebuddy.ui.theme.LightGrey
@@ -26,25 +25,17 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.random.Random
 
-val options = arrayOf(
-    "Got it.",
-    "Thanks buddy.",
-    "..."
-)
 
-val defaultMessages = listOf(
-    "\nHonk Honk Honk!\n",
-    "I may look cute but we are dangerous creatures! \nRespect our space",
-    "\nDon't Policy 71!\n",
-    "\nPhysics has a lot of study spaces!\n",
-    "\nDon't forget your iClicker!\n",
-    "\nBe nice!\n",
-    "\nThank Mr. Goose\n",
-    "\nBe sure to try different clubs! \n here is a variety of things to try!",
-)
 
 @Composable
 fun Home(db:AppDatabase) {
+    val viewModel by remember {
+        mutableStateOf(HomeViewModel(db))
+    }
+
+    val message by viewModel.message.observeAsState(viewModel.getDefaultGooseMessage())
+    val (time, greeting) = viewModel.getGreetingMessageAndTime()
+
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
@@ -53,28 +44,13 @@ fun Home(db:AppDatabase) {
             .background(LightGrey),
     ) {
         Spacer(modifier = Modifier.size(30.dp))
-        DatedGreeting()
+        DatedGreeting(time, greeting)
         Spacer(modifier = Modifier.size(70.dp))
-        val text = remember { mutableStateOf(getDefaultGooseMessage(db)) }
-        GooseAndSpeechBubble(text.value)
-        SpeechOptions(options = options, text, defaultMessages)
+        GooseAndSpeechBubble(message)
+        SpeechOptions(options = viewModel.options, viewModel.defaultMessages, viewModel)
     }
 }
 
-private fun getDefaultGooseMessage(db: AppDatabase): String {
-    var calendarDao = db.CalendarItemDao()
-    var current = kotlinx.datetime.LocalDate.now()
-    var calendarItemsToday = calendarDao.getOnDate(current)
-    var text = "\nHonk Honk!\n"
-    run breaking@
-    {
-        calendarItemsToday.forEach { calendarItem ->
-            text = "Honk! You have ${calendarItem.title} from \n ${calendarItem.startTime} - ${calendarItem.endTime}"
-            return@breaking
-        }
-    }
-    return text
-}
 @Composable
 fun GooseAndSpeechBubble(text: String){
     Column(
@@ -91,23 +67,7 @@ fun GooseAndSpeechBubble(text: String){
 }
 
 @Composable
-fun DatedGreeting() {
-    val current = LocalDateTime.now()
-    val hour = current.hour
-    val formatter = DateTimeFormatter.ofPattern("LLL dd, yyyy")
-    val formatted = current.format(formatter)
-
-    var greeting = "Good Morning!"
-    println(hour)
-    if (hour in 0..11){
-        greeting = "Good Morning!"
-    } else if(hour in 12..16){
-        greeting = "Good Afternoon!"
-    } else if(hour in 17..20){
-        greeting = "Good Evening!"
-    } else if(hour in 21..23){
-        greeting = "Good Night!"
-    }
+fun DatedGreeting(formattedTime: String, greeting: String) {
 
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
@@ -115,13 +75,13 @@ fun DatedGreeting() {
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        Text(text = formatted, fontSize = 24.sp, fontWeight = FontWeight.Medium)
+        Text(text = formattedTime, fontSize = 24.sp, fontWeight = FontWeight.Medium)
         Text(greeting)
     }
 }
 
 @Composable
-fun SpeechOptions(options: Array<String>, text: MutableState<String>, defaultMessages: List<String>) {
+fun SpeechOptions(options: Array<String>, defaultMessages: List<String>, viewModel: HomeViewModel) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -130,8 +90,8 @@ fun SpeechOptions(options: Array<String>, text: MutableState<String>, defaultMes
         options.forEach { it ->
             Button(
                 onClick = {
-                    val randomIndex = Random.nextInt(defaultMessages.size);
-                    text.value = defaultMessages[randomIndex]
+                    val randomIndex = Random.nextInt(defaultMessages.size)
+                    viewModel.updateMessage(defaultMessages[randomIndex])
                   },
                 colors = ButtonDefaults.buttonColors(backgroundColor = Beige),
                 modifier = Modifier
@@ -141,5 +101,4 @@ fun SpeechOptions(options: Array<String>, text: MutableState<String>, defaultMes
             }
         }
     }
-
 }
---
+++ app/src/main/java/com/example/goosebuddy/ui/screens/Home/HomeViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.goosebuddy.ui.screens.Home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.goosebuddy.AppDatabase
+import com.example.goosebuddy.ui.screens.Utility.formatTime
+import io.github.boguszpawlowski.composecalendar.kotlinxDateTime.now
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.time.Duration
+
+class HomeViewModel(db: AppDatabase): ViewModel() {
+    private var _db: AppDatabase
+    init {
+        _db = db
+    }
+    val options = arrayOf(
+        "Got it.",
+        "Thanks buddy.",
+        "..."
+    )
+
+    val defaultMessages = listOf(
+        "\nHonk Honk Honk!\n",
+        "I may look cute but we are dangerous creatures! \nRespect our space",
+        "\nDon't Policy 71!\n",
+        "\nPhysics has a lot of study spaces!\n",
+        "\nDon't forget your iClicker!\n",
+        "\nBe nice!\n",
+        "\nThank Mr. Goose\n",
+        "\nBe sure to try different clubs! \n here is a variety of things to try!",
+    )
+
+    private val _message = MutableLiveData(getDefaultGooseMessage())
+    val message: LiveData<String> = _message
+
+    fun getDefaultGooseMessage(): String {
+        var calendarDao = _db.CalendarItemDao()
+        var current = kotlinx.datetime.LocalDate.now()
+        var calendarItemsToday = calendarDao.getOnDate(current)
+        var text = "\nHonk Honk!\n"
+        run breaking@
+        {
+            calendarItemsToday.forEach { calendarItem ->
+                text = "Honk! You have ${calendarItem.title} from \n ${calendarItem.startTime} - ${calendarItem.endTime}"
+                return@breaking
+            }
+        }
+        return text
+    }
+
+    fun getGreetingMessageAndTime(): Pair<String, String> {
+        val current = LocalDateTime.now()
+        val hour = current.hour
+        val formatter = DateTimeFormatter.ofPattern("LLL dd, yyyy")
+        val formatted = current.format(formatter)
+
+        var greeting = "Good Morning!"
+        println(hour)
+        if (hour in 0..11) {
+            greeting = "Good Morning!"
+        } else if (hour in 12..16) {
+            greeting = "Good Afternoon!"
+        } else if (hour in 17..20) {
+            greeting = "Good Evening!"
+        } else if (hour in 21..23) {
+            greeting = "Good Night!"
+        }
+
+        return Pair(formatted, greeting)
+    }
+
+    fun updateMessage(message: String) {
+        println("UPDATING MESSAGE")
+        _message.value = message
+    }
+}
